61c645b67db0dcd5c3aae9e5f4956d559a06c68b
Bjarte Aarmo Lund <bjarte@smartdok.com>
2025-06-08 19:36:56 +0200

    Refactor database connection setup in AppHost to use preconfigured database (on free tier)

--- GrendeHusCafeAppHost1/AppHost.cs
+++ GrendeHusCafeAppHost1/AppHost.cs
@@ -2,15 +2,13 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sql = builder.AddAzureSqlServer("grendehus-cafe-sql-server");
+var db = builder.AddConnectionString("grendehus-cafe-sql-server");
 
-var db = sql.AddDatabase("grendehus-cafe-db");
 
 builder.AddProject<MigrationService>("grendehus-cafe-migrations")
-    .WithReference(db)
-    .WaitFor(db);
+    .WithReference(db);
 
 builder.AddProject<App>("grendehus-cafe-app")
-    .WithReference(db).WaitFor(db).WithExternalHttpEndpoints();
+    .WithReference(db).WithExternalHttpEndpoints();
 
 builder.Build().Run();
